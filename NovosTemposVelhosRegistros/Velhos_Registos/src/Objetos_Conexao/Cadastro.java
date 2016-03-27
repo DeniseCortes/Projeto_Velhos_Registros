1dc4ab74cc3974fcfc6fe42dd48e87675b919fd9
DeniseCortes <denizecortes@gmail.com>
2016-03-27 08:45:09 -0300

    Foi adicionado um método para cadastrar professor, coordenador e turma.

--- NovosTemposVelhosRegistros/Velhos_Registos/src/Objetos_Conexao/Cadastro.java
+++ NovosTemposVelhosRegistros/Velhos_Registos/src/Objetos_Conexao/Cadastro.java
@@ -6,6 +6,9 @@
 package Objetos_Conexao;
 
 import Objetos_Entidades.Aluno;
+import Objetos_Entidades.Coordenador;
+import Objetos_Entidades.Professor;
+import Objetos_Entidades.Turma;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,17 +20,9 @@ import java.sql.Statement;
  * @author rodolfo
  */
 public class Cadastro {
-    Connection con;
-    Statement comando;
-    ResultSet resultado;
-    
+    Connection con;   
     public Cadastro(){
         con = new Conexao().Conectando();
-        try {
-            comando = con.createStatement();
-        } catch (SQLException ex) {
-            System.out.println("Problema na conexão!");
-        }
     }
     public void Cadastrar_Aluno(int cod_aluno, int senha, String nome, int num_faltas, String atestado, String fixa_med ) throws SQLException{
             Aluno aluno = new Aluno(cod_aluno,
@@ -47,4 +42,56 @@ public class Cadastro {
             comand_prepar.execute();
             con.close();
         }
+    public void Cadastrar_Prof(int cod_prof, int senha_prof, String nome, String ativ_ministrada, int carga_horaria) throws SQLException{
+        Professor prof = new Professor(
+                cod_prof,
+                senha_prof,
+                nome,
+                ativ_ministrada,
+                carga_horaria
+        );
+        
+        PreparedStatement comand_prepar = con.prepareStatement("INSERT INTO professor VALUES(?,?,?,?,?);");
+        comand_prepar.setString(1, Integer.toString(prof.getCod_prof()));
+        comand_prepar.setString(2, Integer.toString(prof.getSenha_prof()));
+        comand_prepar.setString(3, prof.getNome());
+        comand_prepar.setString(4, prof.getAtiv_ministrada());
+        comand_prepar.setString(5,Integer.toString(prof.getCarga_horaria()));
+        comand_prepar.execute();
+        con.close();
+    }
+    public void Cadastrar_Coordenador(String nome, int cod_coordenador, int turma, int senha_coord) throws SQLException{
+        Coordenador coord = new Coordenador(
+                nome,
+                cod_coordenador,
+                turma,
+                senha_coord
+        );
+        
+        PreparedStatement comand_prepar = con.prepareStatement("INSERT INTO coordenador VALUES(?,?,?,?);");
+        comand_prepar.setString(1,coord.getNome());
+        comand_prepar.setString(2,Integer.toString(coord.getCod_coordenador()));
+        comand_prepar.setString(3,Integer.toString(coord.getCod_turma()));
+        comand_prepar.setString(4,Integer.toString(coord.getSenha_coord()));
+        comand_prepar.execute();
+        con.close();
+    
+    }
+    public void Cadastrar_turma(int cod_turma, int quant_alunos, int horario, int aluno, int prof) throws SQLException{
+        Turma turma = new Turma(
+                cod_turma,
+                quant_alunos,
+                horario,
+                aluno,
+                prof
+        );
+        PreparedStatement comand_prepar = con.prepareStatement("INSERT INTO turma VALUES (?,?,?,?,?);");
+        comand_prepar.setString(1, Integer.toString(turma.getCod_turma()));
+        comand_prepar.setString(2, Integer.toString(turma.getQuant_alunos()));
+        comand_prepar.setString(3, Integer.toString(turma.getHorario()));
+        comand_prepar.setString(4, Integer.toString(turma.getAluno()));
+        comand_prepar.setString(5, Integer.toString(turma.getProf()));
+        comand_prepar.execute();
+        con.close();    
+    }
 } 
