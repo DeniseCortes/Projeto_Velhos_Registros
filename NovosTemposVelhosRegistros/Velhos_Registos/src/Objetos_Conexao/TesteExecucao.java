35712f395d7c4bda17182da5a22567269d430ffb
DeniseCortes <denizecortes@gmail.com>
2016-03-27 08:50:55 -0300

    Classe de teste de execução.

---
+++ NovosTemposVelhosRegistros/Velhos_Registos/src/Objetos_Conexao/TesteExecucao.java
@@ -0,0 +1,9 @@
+package Objetos_Conexao;
+import Interfaces.TelaEscolhaLogin;
+
+public class TesteExecucao {
+    
+    public static void main(String[] args){
+        new TelaEscolhaLogin().setVisible(true);
+    }
+}
